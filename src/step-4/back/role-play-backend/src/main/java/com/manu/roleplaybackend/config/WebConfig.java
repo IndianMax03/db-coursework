5f304b6e83f62974d0f86f61e93253a5f07d4e06
IndianMax03 <vermamanu@mail.ru>
2023-12-13 22:43:57 +0300

    Add: security logic

---
+++ src/step-4/back/role-play-backend/src/main/java/com/manu/roleplaybackend/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.manu.roleplaybackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.manu.roleplaybackend.controllers.Interceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new Interceptor());
+    }
+
+}
---
+++ src/step-4/back/role-play-backend/src/main/java/com/manu/roleplaybackend/controllers/Interceptor.java
@@ -0,0 +1,39 @@
+package com.manu.roleplaybackend.controllers;
+
+import java.util.Set;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Interceptor implements HandlerInterceptor {
+
+    private final static Set<String> tokens = new HashSet<>();
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (tokens.isEmpty()) {
+            tokens.add("123");
+        }
+        if (request.getHeader("token") != null && tokens.contains(request.getHeader("token").toString())) {
+            return true;
+        }
+        response.setStatus(403);
+        response.getWriter().write("Permission denied");
+        return false;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+    
+    
+
+}
