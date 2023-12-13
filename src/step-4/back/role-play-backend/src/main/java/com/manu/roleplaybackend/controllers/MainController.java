2c69cac66d86df47a3fe0fb9f14bf647d20296f4
IndianMax03 <vermamanu@mail.ru>
2023-12-13 17:52:29 +0300

    Update: rest controller file

--- src/step-4/back/role-play-backend/src/main/java/com/manu/roleplaybackend/controllers/MainController.java
+++ src/step-4/back/role-play-backend/src/main/java/com/manu/roleplaybackend/controllers/MainController.java
@@ -1,6 +1,13 @@
 package com.manu.roleplaybackend.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Optional;
+
+import com.manu.roleplaybackend.db.DataBase;
+import com.manu.roleplaybackend.model.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class MainController {
 
+    @Autowired
+    private DataBase dataBase;
+
     public MainController() { }
-    
-    @GetMapping("ready")
-    public String getName() {
-        return "ready";
+
+    @PostMapping("add")
+    public Optional<User> addUser(@RequestBody User user) {
+        System.out.println(user);
+        dataBase.addUser(user);
+        return Optional.of(user);
     }
 
 }
