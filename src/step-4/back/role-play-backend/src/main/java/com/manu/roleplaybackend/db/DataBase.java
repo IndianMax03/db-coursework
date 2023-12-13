8be7630a07c12776a7464d40381f20f711e33c49
IndianMax03 <vermamanu@mail.ru>
2023-12-13 17:48:34 +0300

    Add: database classes

---
+++ src/step-4/back/role-play-backend/src/main/java/com/manu/roleplaybackend/db/DataBase.java
@@ -0,0 +1,25 @@
+package com.manu.roleplaybackend.db;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import com.manu.roleplaybackend.model.User;
+
+@Component
+public class DataBase {
+
+    @Autowired
+    JdbcTemplate template;
+    
+    public DataBase() {
+
+    }
+
+    public boolean addUser(User user) {
+        String slq = "insert into users values (?, ?, ?, ?, ?, ?, ?, ?, cast(? as user_status))";
+        template.update(slq, user.getId(), user.getLogin(), user.getName(), user.getPassword(), user.getKarma(), user.getTimezone(), user.getTelegramTag(), user.getVkTag(), user.getUserStatus());
+        return true;
+    }
+
+}
