c477b6475f2f393be829ae120904024309268993
IndianMax03 <vermamanu@mail.ru>
2023-12-13 22:43:26 +0300

    Update: main controller

--- src/step-4/back/role-play-backend/src/main/java/com/manu/roleplaybackend/controllers/MainController.java
+++ src/step-4/back/role-play-backend/src/main/java/com/manu/roleplaybackend/controllers/MainController.java
@@ -6,6 +6,7 @@ import com.manu.roleplaybackend.db.DataBase;
 import com.manu.roleplaybackend.model.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,11 @@ public class MainController {
     private DataBase dataBase;
 
     public MainController() { }
+
+    @GetMapping("ping")
+    public String ping() {
+        return "pong";
+    }
 
     @PostMapping("add")
     public Optional<User> addUser(@RequestBody User user) {
