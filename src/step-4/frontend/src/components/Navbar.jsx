00b1680b2f1402da7e50b39a5a4572b9c8b1b222
Dead <space.gee.z@gmail.com>
2023-12-14 12:30:13 +0300

    add icons to navbar

--- src/step-4/frontend/src/components/Navbar.jsx
+++ src/step-4/frontend/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import { Link } from 'react-router-dom';
+import { CgProfile } from "react-icons/cg";
+import { GrCatalog } from "react-icons/gr";
+import { FaUserFriends } from "react-icons/fa";
+import { CiLogout } from "react-icons/ci";
+
+
 
 const Navbar = () => {
   return (
     <div className="grid h-32 sticky top-10">
-      <Link to={'/profile'}>Мой профиль</Link>
-      <Link to={'/catalog'}>Каталог</Link>
-      <Link to={'/friends'}>Друзья</Link>
-      <Link to={'/login'}>Выйти</Link>
+      <Link to={'/profile'} className='flex space-x-4'><CgProfile  className='h-6'/><div>Мой профиль</div></Link>
+      <Link to={'/catalog'} className='flex space-x-4'><GrCatalog className='h-6'/><div>Каталог</div></Link>
+      <Link to={'/friends'} className='flex space-x-4'><FaUserFriends className='h-6'/><div>Друзья</div></Link>
+      <Link to={'/login'} className='flex space-x-4'><CiLogout className='h-6'/><div>Выйти</div></Link>
     </div>
   );
 };
