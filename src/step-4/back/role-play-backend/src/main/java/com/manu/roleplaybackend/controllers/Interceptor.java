a553e16a92ea73dd1c811ee1b96a344dfd9ec46f
IndianMax03 <vermamanu@mail.ru>
2023-12-14 21:39:44 +0300

    Update: handler interceptor logic

--- src/step-4/back/role-play-backend/src/main/java/com/manu/roleplaybackend/controllers/Interceptor.java
+++ src/step-4/back/role-play-backend/src/main/java/com/manu/roleplaybackend/controllers/Interceptor.java
@@ -3,6 +3,7 @@ package com.manu.roleplaybackend.controllers;
 import java.util.Set;
 import java.util.HashSet;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -12,23 +13,37 @@ import jakarta.servlet.http.HttpServletResponse;
 public class Interceptor implements HandlerInterceptor {
 
     private final static Set<String> tokens = new HashSet<>();
+    private final static String registerURI = "/enter/register";
+    private final static String loginURI = "/enter/login";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (tokens.isEmpty()) {
             tokens.add("123");
         }
-        if (request.getHeader("token") != null && tokens.contains(request.getHeader("token").toString())) {
+        if (
+            request.getHeader("token") != null &&
+            tokens.contains(request.getHeader("token").toString()) ||
+            request.getRequestURI().equals(registerURI) && request.getMethod().equals("POST") ||
+            request.getRequestURI().equals(loginURI) && request.getMethod().equals("POST")
+            ) {
             return true;
         }
-        response.setStatus(403);
-        response.getWriter().write("Permission denied");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(HttpStatus.UNAUTHORIZED.getReasonPhrase());
         return false;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        if (
+            request.getRequestURI().equals(registerURI) && request.getMethod().equals("POST") ||
+            request.getRequestURI().equals(loginURI) && request.getMethod().equals("POST")
+            ) {
+                if (response.containsHeader("token")) {
+                    tokens.add(response.getHeader("token"));
+                }
+            }
     }
     
     
