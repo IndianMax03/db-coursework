3f2ccf6ff301dc269e88262cc66d89f2d2dd021d
Dead <space.gee.z@gmail.com>
2023-12-14 15:46:26 +0300

    add character selection

---
+++ src/step-4/frontend/src/components/CharacterSelection.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import Character from './Character';
+
+const CharacterSelection = ({characters}) => {
+  return (
+    <div className="flex border-solid border-2 border-slate-500 rounded-lg">
+       {characters.map((character) => (character))}
+    </div>
+  );
+};
+
+export default CharacterSelection;
