883cbd254a2b3e6d21abe0fdb650b34c1a68a9aa
IndianMax03 <vermamanu@mail.ru>
2023-12-15 01:41:11 +0300

    Delete: comments

--- src/step-4/back/role-play-backend/src/main/java/com/manu/roleplaybackend/controllers/EnterController.java
+++ src/step-4/back/role-play-backend/src/main/java/com/manu/roleplaybackend/controllers/EnterController.java
@@ -5,7 +5,6 @@ import com.manu.roleplaybackend.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,37 +20,13 @@ public class EnterController {
     public EnterController() { }
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody User user) {
+    public ResponseEntity<Object> register(@RequestBody User user) {
         return userService.register(user);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody User user) {
+    public ResponseEntity<Object> login(@RequestBody User user) {
         return userService.login(user);
     }
-
-    @GetMapping("/ping")
-    public String ping() {
-        return "pong";
-    }
-
-    // @GetMapping("gimmeUsers")
-    // public Iterable<User> gimmeUsers() {
-    //     return dataBase.getUsers();
-    // }
-
-    // @PostMapping("add")
-    // public Optional<User> addUser(@RequestBody User user) {
-    //     System.out.println(user);
-    //     dataBase.addUser(user);
-    //     return Optional.of(user);
-    // }
-
-    // @PostMapping("addGame")
-    // public Optional<Game> addGame(@RequestBody Game game) {
-    //     System.out.println(game);
-    //     dataBase.addGame(game);
-    //     return Optional.of(game);
-    // }
 
 }
