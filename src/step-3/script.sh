bdc79142e2859b08066c97fb5f3e81182d74ea8d
IndianMax03 <vermamanu@mail.ru>
2024-10-02 00:38:18 +0300

    Refactor: repo structure

--- src/step-3/script.sh
+++
@@ -1,6 +0,0 @@
-psql -U postgres -d db_coursework -a -f ./drop_tables.sql
-psql -U postgres -d db_coursework -a -f ./drop_types.sql
-psql -U postgres -d db_coursework -a -f ./model.sql
-psql -U postgres -d db_coursework -a -f ./functions.sql
-psql -U postgres -d db_coursework -a -f ./triggers.sql
-psql -U postgres -d db_coursework -a -f ./insertions.sql
